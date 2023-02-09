988ca09f12f7ea10fc08ecd8f984525683245fa4
Munteanu <munteanuvictoria1@gmail.com>
2023-02-09 16:22:45 +0100

    update

--- src/script.js
+++ src/script.js
@@ -1,12 +1,22 @@
 
-
 //Arrays 
-let Planets= ['Moon', 'Earth']
+//let Planets= ['Moon', 'Earth']
 
 
 //DOM 
 
 
+//examine the docoument object 
+console.dir(document); 
+console.log(document.domain)
+console.log(document.URL)
+console.log(document.title)
+console.log(document.doctype)
+console.log(document.body)
+console.log(document.all)
+console.log(document.all[10])
+document.all[10].textContent = 'Hello'
 
-//Loops 
+
+
 
