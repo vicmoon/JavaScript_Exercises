7bda3ef4b6f8063441870507ad70008bd5584432
Munteanu <munteanuvictoria1@gmail.com>
2023-02-09 16:58:24 +0100

    update

--- src/script.js
+++ src/script.js
@@ -15,7 +15,34 @@ console.log(document.doctype)
 console.log(document.body)
 console.log(document.all)
 console.log(document.all[10])
-document.all[10].textContent = 'Hello'
+//document.all[10].textContent = 'Hello'
+console.log(document.forms[0])
+console.log(document.links)
+console.log(document.images)
+
+// get element by ID 
+
+console.log(document.getElementById('header'));
+let header = document.getElementById('header'); 
+console.log(header); 
+header.textContent= 'Hello';
+header.innerText = 'Goodbye';
+header.innerHTML= '<h3> Hello <h3>'; 
+
+header.style.borderBottom = 'solid 3px red'
+
+// get elements by Class name 
+
+let items = document.getElementsByClassName('items');
+console.log(items);
+items[1].style.backgroundColor =  'yellow';
+items[2].textContent = 'Hello from here';
+items[0].style.fontWeight = 'bold' ;
+
+for(let  i=0; i < items.length; i++){
+    items[i].style.backgroundColor = '#358946';
+}
+
 
 
 
